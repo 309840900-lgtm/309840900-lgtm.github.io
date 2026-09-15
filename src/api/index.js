56f16a8b7ce6b89c22e2fcfc801d1ef24b95e0a5
Lin_Yu <309840900@qq.com>
2026-09-15 12:58:33 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -5,12 +5,10 @@ import fetchJsonp from "fetch-jsonp";
  * 音乐播放器
  */
 
-// 获取音乐播放列表
-export const getPlayerList = async (server, type, id) => {
-  const res = await fetch(
-    `${import.meta.env.VITE_SONG_API}?server=${server}&type=${type}&id=${id}`,
-  );
-  const data = await res.json();
+    export const getPlayerList = (server, type, id) => {
+     return fetch(`${import.meta.env.VITE_SONG_API}?server=${server}&type=${type}&id=${id}`)
+     .then(res => res.json())
+   }
 
   if (data[0].url.startsWith("@")) {
     // eslint-disable-next-line no-unused-vars
