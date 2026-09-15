e8e2d417dd459e9cd356b583cb69d595e0a0ed40
Lin_Yu <309840900@qq.com>
2026-09-15 23:03:20 +0800

    Create projects.js

---
+++ src/data/projects.js
@@ -0,0 +1,19 @@
+export const projectsList = [
+  {
+    id: 1,
+    name: "项目名称1",
+    desc: "一句话描述项目功能和特点",
+    cover: "项目封面图链接",
+    github: "https://github.com/你的用户名/项目仓库",
+    tags: ["Vue", "前端"]
+  },
+  {
+    id: 2,
+    name: "项目名称2",
+    desc: "一句话描述项目功能和特点",
+    cover: "项目封面图链接",
+    github: "https://github.com/你的用户名/项目仓库",
+    tags: ["工具", "全栈"]
+  },
+  // 继续往下加就行
+]
